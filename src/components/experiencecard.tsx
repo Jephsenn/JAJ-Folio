9cf71e6c5ebba1b08e927bce5372e3b4d0a01c05
John Josephsen <jjosephsenyt@gmail.com>
2023-04-13 09:29:22 -0400

    styling changes

--- src/components/experiencecard.tsx
+++ src/components/experiencecard.tsx
@@ -8,10 +8,11 @@ type Props = {
 
 export default function ExperienceCard( props: Props ) {
     return (
-      <a href={props.link} target="_blank" className="group m-4 md:w-1/4 h-44 md:h-52 relative bg-white">
+      <a href={props.link==="#"?"javascript:void(0)":props.link} target={props.link==="#"?"_self":"_blank"} className="group m-4 md:w-1/4 h-44 md:h-52 relative bg-white">
         <Image className="h-full object-cover ease-in-out duration-300 group-hover:cursor-pointer group-hover:opacity-20" src={props.src} alt={props.alt}/>
         <div className="opacity-0 absolute ease-in-out duration-300 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 group-hover:opacity-100">
             <p className="text-black text-lg">{props.alt}</p>
+            {props.link==="#"?<p className="text-red text-lg">No Longer Live</p>:""}
         </div>
       </a>
     )
